b4c4afa6faf32f87f020272f73ea5ee2069664b1
Hyeonjong <nianelo4@gmail.com>
2022-07-19 21:53:24 +0900

    Apply eslint manually

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,28 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb",
-        "plugin:react/jsx-runtime",
-        "airbnb-typescript",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json",
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/brace-style": [2, "stroustrup"],
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/brace-style': [2, 'stroustrup'],
+  },
+};
