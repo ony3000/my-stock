3d29a62d777d15d68e3e69ba2c6cac691f7157c8
Hyeonjong <nianelo4@gmail.com>
2022-08-03 23:17:35 +0900

    Add first migration file

--- backend/mystock/settings.py
+++ backend/mystock/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "mystock.market.apps.MarketConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
