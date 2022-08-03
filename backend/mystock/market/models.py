ee9e2635f361c22cfa8cf3550489d78c9c0eb5e1
Hyeonjong <nianelo4@gmail.com>
2022-08-03 23:34:30 +0900

    Register stock model in django admin

--- backend/mystock/market/models.py
+++ backend/mystock/market/models.py
@@ -5,3 +5,6 @@ from mystock.core.models import BaseModel
 class Stock(BaseModel):
     code = models.CharField(max_length=8, unique=True)
     kr_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.kr_name} ({self.code})"
