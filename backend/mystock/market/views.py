3b80cbbd4b45ca728ab31fb4ba1d4d91c76744d9
Hyeonjong <nianelo4@gmail.com>
2022-08-04 23:33:14 +0900

    Define viewset for stock model

--- backend/mystock/market/views.py
+++ backend/mystock/market/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from mystock.market.models import Stock
+from mystock.market.serializers import StockSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all().order_by("id")
+    serializer_class = StockSerializer
