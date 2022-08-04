f2a51e37480633761f09e15fb3621a585339ed5e
Hyeonjong <nianelo4@gmail.com>
2022-08-04 23:36:46 +0900

    Add API URLs

--- backend/mystock/urls.py
+++ backend/mystock/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from mystock.market import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"stocks", views.StockViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
 ]
