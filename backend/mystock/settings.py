b421efd2081654ca6a54e4c1e7bada2c24b92ca1
Hyeonjong <nianelo4@gmail.com>
2022-08-13 22:30:26 +0900

    Update settings.py

--- backend/mystock/settings.py
+++ backend/mystock/settings.py
@@ -152,6 +152,6 @@ REST_FRAMEWORK = {
 
 # Django CORS headers
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
