330035a14a3f90577d27d42e9b58c7843889f684
Hyeonjong <nianelo4@gmail.com>
2022-08-13 18:18:23 +0900

    Prepare structure similar to existing project

---
+++ temp-frontend/app/plugins/axios.ts
@@ -0,0 +1,61 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import {
+  ErrorResponseData, ErrorInformation, RefinedResponse, RefinedError,
+} from '~/types/apis';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8000/api/v1/',
+  headers: {
+    Accept: 'application/json',
+  },
+});
+
+instance.interceptors.response.use(
+  (response: AxiosResponse<unknown>) => ({
+    status: response.status,
+    data: response.data,
+  }),
+  (error: AxiosError<ErrorResponseData>) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    const errorInfo = error.toJSON() as ErrorInformation;
+    const refinedError: RefinedError = error.response ? {
+      status: error.response.status,
+      message: (
+        typeof error.response.data === 'object'
+          ? error.response.data.detail
+          : (error.response.statusText || error.message)
+      ),
+    } : {
+      status: Number(errorInfo.status),
+      message: errorInfo.message,
+    };
+
+    return refinedError;
+  },
+);
+
+export const typedGet = async <T = unknown>(...params: Parameters<typeof axios.get>) => {
+  const typedResult = await instance.get<T>(...params) as RefinedResponse<T> | RefinedError;
+
+  return Promise.resolve(typedResult);
+};
+
+export const typedPost = async <T = unknown>(...params: Parameters<typeof axios.post>) => {
+  const typedResult = await instance.post<T>(...params) as RefinedResponse<T> | RefinedError;
+
+  return Promise.resolve(typedResult);
+};
+
+export const typedPatch = async <T = unknown>(...params: Parameters<typeof axios.patch>) => {
+  const typedResult = await instance.patch<T>(...params) as RefinedResponse<T> | RefinedError;
+
+  return Promise.resolve(typedResult);
+};
+
+export const typedDelete = async <T = unknown>(...params: Parameters<typeof axios.delete>) => {
+  const typedResult = await instance.delete<T>(...params) as RefinedResponse<T> | RefinedError;
+
+  return Promise.resolve(typedResult);
+};
--- temp-frontend/app/routes/index.tsx
+++ temp-frontend/app/routes/index.tsx
@@ -1,32 +1,55 @@
+import { useState, useEffect } from 'react';
+import { ListApiResponse } from '~/types/apis';
+import { Stock } from '~/types/models';
+import { typedGet, typedPost } from '~/plugins/axios';
+
 export default function Index() {
+  const [stocks, setStocks] = useState<Stock[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data, message } = await typedGet<ListApiResponse<Stock>>('stocks/');
+
+      if (message) {
+        // error handling
+      }
+
+      if (data) {
+        setStocks(data.results);
+      }
+    })();
+  }, []);
+
+  const getApi = async () => {
+    const response = await typedGet('random-400/');
+
+    console.log(response);
+  };
+
+  const postApi = async () => {
+    const response = await typedPost('random-500/');
+
+    console.log(response);
+  };
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div>
+        <button type="button" onClick={getApi}>Randomly returns 200 or 4xx</button>
+        <button type="button" onClick={postApi}>Randomly returns 204 or 5xx</button>
+        <ul>
+          {stocks.map((stock) => (
+            <li key={stock.code}>
+              {stock.krName}
+              {' '}
+              (
+              {stock.code}
+              )
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
