a2f6e477d1b023bacecd0144fd98987ada4148f3
Hyeonjong <nianelo4@gmail.com>
2022-09-04 22:54:53 +0900

    Revert "Ensures that values with Korean are sorted as expected"
    This reverts commit 513a08e9d8969b78aa4025a791b4a0fe763d160c.

--- backend/mystock/market/views.py
+++ backend/mystock/market/views.py
@@ -1,4 +1,3 @@
-from django.db.models.functions import Collate
 from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
 from mystock.market.models import Stock
@@ -18,7 +17,7 @@ class StockViewSet(viewsets.ReadOnlyModelViewSet):
     )
     @action(detail=False)
     def increasing(self, request):
-        stocks = Stock.objects.all().order_by(Collate("kr_name", "C").desc())
+        stocks = Stock.objects.all().order_by("-kr_name")
 
         page = self.paginate_queryset(stocks)
         if page is not None:
@@ -34,7 +33,7 @@ class StockViewSet(viewsets.ReadOnlyModelViewSet):
     )
     @action(detail=False)
     def decreasing(self, request):
-        stocks = Stock.objects.all().order_by(Collate("kr_name", "C"))
+        stocks = Stock.objects.all().order_by("kr_name")
 
         page = self.paginate_queryset(stocks)
         if page is not None:
