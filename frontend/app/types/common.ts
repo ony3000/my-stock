fa53af2054efdae0df0042363876eff62eae8e0a
Hyeonjong <nianelo4@gmail.com>
2022-09-10 01:28:52 +0900

    Fix decimal pattern type

--- frontend/app/types/common.ts
+++ frontend/app/types/common.ts
@@ -4,6 +4,6 @@ export type Nullable<T> = T | null;
 
 export type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-export type DecimalPattern = `${number}.${Digit}${Digit}`;
+export type DecimalPattern = `${bigint}.${Digit}${Digit}`;
 
 export type UrlPattern = `http${'s' | ''}://${string}`;
