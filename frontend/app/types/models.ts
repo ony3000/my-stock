a1a3b42ff9e1cb6a73ee8cd76a9ffc49161efeb3
Hyeonjong <nianelo4@gmail.com>
2022-10-03 00:38:39 +0900

    Define searchable stock type

--- frontend/app/types/models.ts
+++ frontend/app/types/models.ts
@@ -4,8 +4,14 @@ export type Stock = {
   code: string;
   logoImage: Nullable<UrlPatternString>;
   krName: string;
-  krNameInitialConsonant: string;
   usName: string;
   krwPrice: number;
   krwPriceFluctuationRate: DecimalPatternString;
+};
+
+export type SearchableStock = {
+  code: string;
+  logoImage: Nullable<UrlPatternString>;
+  readableName: string;
+  keywords: string[];
 };
