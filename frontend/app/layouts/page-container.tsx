af8c936b55877a3a936d6d02966d5fca5d5a8f68
Hyeonjong <nianelo4@gmail.com>
2022-10-05 23:48:24 +0900

    Update container element

--- frontend/app/layouts/page-container.tsx
+++ frontend/app/layouts/page-container.tsx
@@ -1,11 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import classNames from 'classnames';
 import { ErrorDialog, GlobalNavigation } from '~/components';
 
 export default function PageContainer({
+  className,
   title,
   children,
+  ...otherProps
 }: React.ComponentProps<'div'>) {
   return (
-    <div className="container max-w-screen-sm min-h-screen pb-15 bg-gray-100">
+    <div
+      className={classNames('container max-w-screen-sm min-h-screen pb-15 bg-gray-100', className)}
+      {...otherProps}
+    >
       <h1 className="sr-only">{title}</h1>
       <GlobalNavigation />
       <main>
