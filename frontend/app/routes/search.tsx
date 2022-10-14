639e4eca8803fdc2051b1d8f0de33589777e5bb1
Hyeonjong <nianelo4@gmail.com>
2022-10-15 00:58:45 +0900

    Render search input

--- frontend/app/routes/search.tsx
+++ frontend/app/routes/search.tsx
@@ -1,6 +1,9 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { SearchIcon } from '@heroicons/react/outline';
+import { XCircleIcon } from '@heroicons/react/solid';
+import { useState, useRef } from 'react';
 import invariant from 'tiny-invariant';
 import type { SearchableStock } from '~/types/models';
 import { typedGet } from '~/plugins/axios';
@@ -20,11 +23,35 @@ export const loader: LoaderFunction = async () => {
 
 export default function Search() {
   const searchableStocks = useLoaderData<SearchableStock[]>();
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <PageContainer className="pt-15" title="검색">
       <section className="fixed z-50 top-0 w-full max-w-screen-sm h-15 bg-white">
         <h2 className="sr-only">검색어 입력</h2>
+        <ContentWrapper className="flex items-center h-full">
+          <SearchIcon className="relative shrink-0 w-5 h-5 -mr-6.25 ml-1.25 text-gray-700 pointer-events-none" />
+          <input
+            ref={inputRef}
+            className="w-full h-full px-10 placeholder:text-gray-300"
+            type="text"
+            placeholder="주식명, 주식코드 검색"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          {inputValue && inputRef.current && (
+            <button
+              type="button"
+              className="relative shrink-0 w-5 h-5 mr-1.25 -ml-6.25 rounded-full"
+              onClick={() => {
+                inputRef.current!.value = '';
+                setInputValue('');
+              }}
+            >
+              <XCircleIcon className="w-5 h-5 text-gray-300" />
+            </button>
+          )}
+        </ContentWrapper>
       </section>
       <section className="bg-white">
         <div className="sticky top-15 h-12 pt-3.75 bg-white">
