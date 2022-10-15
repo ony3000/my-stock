2e1c5e77428c201898e133abe3a931fb3267ae05
Hyeonjong <nianelo4@gmail.com>
2022-10-16 00:52:25 +0900

    Render non-empty search results

---
+++ frontend/app/components/search/stock-list.tsx
@@ -0,0 +1,33 @@
+import type { SearchableStock } from '~/types/models';
+import { ContentWrapper } from '~/layouts';
+import { makeGravatarUrl } from '~/utils/gravatar';
+
+type StockListProps = {
+  items: SearchableStock[];
+};
+
+export default function StockList({ items }: StockListProps) {
+  return (
+    <ContentWrapper>
+      <ul>
+        {items.map(({
+          code, logoImage, readableName,
+        }) => (
+          <li key={code} className="flex items-center h-13.75 whitespace-nowrap">
+            <div>
+              <img
+                className="w-7.5 h-7.5 object-contain rounded-full"
+                src={logoImage ?? makeGravatarUrl(code)}
+                alt={`${readableName} 아이콘`}
+              />
+            </div>
+            <div className="flex-1 overflow-hidden flex items-center">
+              <div className="overflow-hidden text-ellipsis ml-2.5 font-semibold">{readableName}</div>
+              <div className="ml-1.5 text-xs text-gray-500">{code}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </ContentWrapper>
+  );
+}
--- frontend/app/routes/search.tsx
+++ frontend/app/routes/search.tsx
@@ -8,15 +8,18 @@ import invariant from 'tiny-invariant';
 import type { SearchableStock } from '~/types/models';
 import { typedGet } from '~/plugins/axios';
 import { PageContainer, ContentWrapper } from '~/layouts';
+import { StockList } from '~/components/search';
 import { isApiError } from '~/utils/type-guard';
-import { makeGravatarUrl } from '~/utils/gravatar';
 
 export const loader: LoaderFunction = async () => {
   const response = await typedGet<SearchableStock[]>('bulk/searchable-stocks/');
 
   invariant(!isApiError(response));
 
-  const searchableStocks = response.data;
+  const searchableStocks = response.data.map<SearchableStock>(({ keywords, ...otherProps }) => ({
+    keywords: keywords.map((keyword) => keyword.toLowerCase()),
+    ...otherProps,
+  }));
 
   return json(searchableStocks);
 };
@@ -25,6 +28,13 @@ export default function Search() {
   const searchableStocks = useLoaderData<SearchableStock[]>();
   const [inputValue, setInputValue] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const lowerCaseInputValue = inputValue.toLowerCase();
+  const filteredStocks = searchableStocks.filter(
+    (stock) => stock.keywords.some(
+      (keyword) => keyword.includes(lowerCaseInputValue),
+    ),
+  );
 
   return (
     <PageContainer className="pt-15" title="검색">
@@ -53,38 +63,28 @@ export default function Search() {
           )}
         </ContentWrapper>
       </section>
-      <section className="bg-white">
-        <div className="sticky top-15 h-12 pt-3.75 bg-white">
-          <ContentWrapper className="flex justify-between items-center h-full">
-            <h2 className="text-[13px] leading-[1.125rem] font-semibold">해외주식</h2>
-            <div className="text-gray-400 text-xs">시가총액순</div>
-          </ContentWrapper>
-        </div>
-        <ContentWrapper>
-          <ul>
-            {searchableStocks.map(({
-              code, logoImage, readableName,
-            }) => (
-              <li key={code} className="flex items-center h-13.75 whitespace-nowrap">
-                <div>
-                  <img
-                    className="w-7.5 h-7.5 object-contain rounded-full"
-                    src={logoImage ?? makeGravatarUrl(code)}
-                    alt={`${readableName} 아이콘`}
-                  />
-                </div>
-                <div className="flex-1 overflow-hidden flex items-center">
-                  <div className="overflow-hidden text-ellipsis ml-2.5 font-semibold">{readableName}</div>
-                  <div className="ml-1.5 text-xs text-gray-500">{code}</div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </ContentWrapper>
-      </section>
-      <section>
-        <h2 className="sr-only">검색 결과</h2>
-      </section>
+      {inputValue ? (
+        <section>
+          <h2 className="sr-only">검색 결과</h2>
+          {filteredStocks.length ? (
+            <StockList items={filteredStocks} />
+          ) : (
+            <ContentWrapper>
+              <div>검색결과 없음</div>
+            </ContentWrapper>
+          )}
+        </section>
+      ) : (
+        <section className="bg-white">
+          <div className="sticky top-15 h-12 pt-3.75 bg-white">
+            <ContentWrapper className="flex justify-between items-center h-full">
+              <h2 className="text-[13px] leading-[1.125rem] font-semibold">해외주식</h2>
+              <div className="text-gray-400 text-xs">시가총액순</div>
+            </ContentWrapper>
+          </div>
+          <StockList items={searchableStocks} />
+        </section>
+      )}
     </PageContainer>
   );
 }
