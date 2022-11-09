d786fc0f0846be188955fe22015e6c1958d5f75a
Hyeonjong <nianelo4@gmail.com>
2022-11-10 00:15:28 +0900

    Add django app

--- backend/mystock/settings.py
+++ backend/mystock/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS: list[str] = []
 
 INSTALLED_APPS = [
     "mystock.market.apps.MarketConfig",
+    "mystock.mock.apps.MarketConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
