a7d3f9c05d6da868e7dd0adefe2527702d37a72d
Hyeonjong <nianelo4@gmail.com>
2022-11-11 21:40:57 +0900

    Define profile model with minimal fields

--- backend/mystock/mock/models.py
+++ backend/mystock/mock/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from mystock.core.models import BaseModel
 
-# Create your models here.
+
+class Profile(BaseModel):
+    name = models.CharField(help_text="이름", max_length=32)
+    phone = models.CharField(help_text="전화번호", max_length=32)
+    email = models.CharField(help_text="이메일", max_length=128)
