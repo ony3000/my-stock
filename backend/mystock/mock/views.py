7632a13aa3240ec74557b3e73812784d550b0cf7
Hyeonjong <nianelo4@gmail.com>
2022-11-11 22:01:45 +0900

    Define viewset for profile model

--- backend/mystock/mock/views.py
+++ backend/mystock/mock/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from mystock.mock.models import Profile
+from mystock.mock.serializers import ProfileSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all().order_by("id")
+    serializer_class = ProfileSerializer
