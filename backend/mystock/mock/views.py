1765ebc3a27c6a29b04653eb0e652f99a4fa0d14
Hyeonjong <nianelo4@gmail.com>
2022-11-12 23:03:16 +0900

    Define permission class for profile viewset

--- backend/mystock/mock/views.py
+++ backend/mystock/mock/views.py
@@ -1,8 +1,21 @@
 from mystock.mock.models import Profile
 from mystock.mock.serializers import ProfileSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.request import Request
+
+
+class ProfilePermission(permissions.BasePermission):
+    def has_permission(self, request: Request, view: "ProfileViewSet") -> bool:  # type: ignore
+        if view.action in ["update", "partial_update", "destroy"]:
+            return request.user.is_staff
+
+        if view.action in ["create"]:
+            return request.user.is_authenticated
+
+        return True
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all().order_by("id")
     serializer_class = ProfileSerializer
+    permission_classes = [ProfilePermission]
