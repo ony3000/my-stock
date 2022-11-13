fc1901ff84019fdce7bb1a8a0626e9bca39ccfd3
Hyeonjong <nianelo4@gmail.com>
2022-11-13 12:34:58 +0900

    Update base button element

--- frontend/app/components/playground/base-button.tsx
+++ frontend/app/components/playground/base-button.tsx
@@ -1,21 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import classNames from 'classnames';
 
-type BaseButtonProps = {
-  className?: string;
+type BaseButtonProps = React.ComponentProps<'button'> & {
   textContent: string;
-  onClick: () => void;
 };
 
 export default function BaseButton({
-  className = '',
+  className,
   textContent,
   onClick,
+  ...otherProps
 }: BaseButtonProps) {
   return (
     <button
       type="button"
       className={classNames('inline-flex justify-center items-center h-10 m-4 px-4 rounded-xl text-white', className)}
       onClick={onClick}
+      {...otherProps}
     >
       {textContent}
     </button>
