48e802058401028f65a385e14c65b39f7f577560
Hyeonjong <nianelo4@gmail.com>
2022-11-17 22:29:22 +0900

    Fix response type of API that returns paginated list

--- frontend/app/components/playground/ordinary-200-button.tsx
+++ frontend/app/components/playground/ordinary-200-button.tsx
@@ -1,10 +1,11 @@
+import type { ListApiResponse } from '~/types/apis';
 import type { Profile } from '~/types/mocks';
 import { typedGet } from '~/plugins/axios';
 import BaseButton from './base-button';
 
 export default function Ordinary200Button() {
   const apiRequest = async () => {
-    const response = await typedGet<Profile[]>('profiles/');
+    const response = await typedGet<ListApiResponse<Profile>>('profiles/');
 
     console.log(response);
   };
